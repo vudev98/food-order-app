11f5d1ee1daed02242c0489116aad2c29ff5bb5d
Nguyen Ngoc Vu <vudev98@gmail.com>
2022-09-10 01:39:07 +0700

    adding a form and fixing input IDs

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from './components/Layout/Header'
+import {Fragment} from "react";
+import Meals from "./components/Meals/Meals";
+
+function App() {
+
+  return(
+      <Fragment>
+          <Header />
+          <main>
+              <Meals />
+          </main>
+      </Fragment>
+  )
+}
+
+export default App;
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,17 @@
+import {Fragment} from "react";
+import mealsImage  from '../../assets/meals.jpg'
+import classes from './Header.module.css'
+import HeaderCartButton from "./HeaderCartButton";
+const Header = props => {
+    return <Fragment>
+            <header className={classes.header}>
+                <h1>ReactMeals</h1>
+            <HeaderCartButton />
+            </header>
+            <div className={classes['main-image']}>
+                <img src={mealsImage} alt="meal-img" />
+            </div>
+    </Fragment>
+};
+
+export default Header
