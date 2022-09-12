c2741c7e6586c4500593de833cd1cb06fb22e843
Nguyen Ngoc Vu <vudev98@gmail.com>
2022-09-12 07:13:03 +0700

    Managing Cart and Modal State

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import Header from './components/Layout/Header'
-import {Fragment} from "react";
+import Header from "./components/Layout/Header";
+import { Fragment, useState } from "react";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
+	const [cartIsShown, setCartIsShown] = useState(false);
 
-  return(
-      <Fragment>
-          <Header />
-          <main>
-              <Meals />
-          </main>
-      </Fragment>
-  )
+	const showCartHandler = () => {
+		setCartIsShown(true);
+	};
+
+	const hideCartHandler = () => {
+		setCartIsShown(false);
+	};
+
+	return (
+		<Fragment>
+			{cartIsShown && <Cart onClose={hideCartHandler} />}
+			<Header onShowCart={showCartHandler} />
+			<main>
+				<Meals />
+			</main>
+		</Fragment>
+	);
 }
 
 export default App;
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,20 @@
-import {Fragment} from "react";
-import mealsImage  from '../../assets/meals.jpg'
-import classes from './Header.module.css'
+import { Fragment } from "react";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
-const Header = props => {
-    return <Fragment>
-            <header className={classes.header}>
-                <h1>ReactMeals</h1>
-            <HeaderCartButton />
-            </header>
-            <div className={classes['main-image']}>
-                <img src={mealsImage} alt="meal-img" />
-            </div>
-    </Fragment>
+
+const Header = (props) => {
+	return (
+		<Fragment>
+			<header className={classes.header}>
+				<h1>ReactMeals</h1>
+				<HeaderCartButton onClick={props.onShowCart} />
+			</header>
+			<div className={classes["main-image"]}>
+				<img src={mealsImage} alt='meal-img' />
+			</div>
+		</Fragment>
+	);
 };
 
-export default Header
+export default Header;
