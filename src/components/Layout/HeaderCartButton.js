9d61efa9033b47612bde0c98c5acea807ff14e16
Nguyen Ngoc Vu <vudev98@gmail.com>
2022-10-02 14:34:18 +0700

    using effectHook to create bump effect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,40 @@
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import CartContext from "../../store/cart-context";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 const HeaderCartButton = (props) => {
-  const cartCtx = useContext(CartContext);
+	const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
-  const numberOfCartItem = cartCtx.items.reduce((currNumber, item) => {
-    return currNumber + item.amount
-  }, 0);
+	const cartCtx = useContext(CartContext);
+
+	const { items } = cartCtx;
+
+	const numberOfCartItem = cartCtx.items.reduce((currNumber, item) => {
+		return currNumber + item.amount;
+	}, 0);
+
+	const btnClasses = `${classes.button} ${
+		btnIsHighlighted ? classes.bump : ""
+	}`;
+
+	useEffect(() => {
+		if (items.length === 0) {
+			return;
+		}
+		setBtnIsHighlighted(true);
+
+		const timer = setTimeout(() => {
+			setBtnIsHighlighted(false);
+		}, 300);
+
+		return () => {
+			clearTimeout(timer);
+		};
+	}, [items]);
 
 	return (
-		<button className={classes.button} onClick={props.onClick}>
+		<button className={btnClasses} onClick={props.onClick}>
 			<span className={classes.icon}>
 				<CartIcon />
 			</span>
